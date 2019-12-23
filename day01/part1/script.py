d454b8505f697f503cc1ebd7a5027b40020cb393
Lynn Kim <lynnkim2000@gmail.com>
2019-12-23 16:15:08 -0500

    init commit

---
+++ day01/part1/script.py
@@ -0,0 +1,11 @@
+import math
+
+file = open('input', 'r')
+
+total_fuel = 0
+
+for x in file:
+    curr = math.floor( int (x) / 3) - 2 
+    total_fuel += curr
+
+print("The total fuel required is", total_fuel)
---
+++ day01/part2/script.py
@@ -0,0 +1,17 @@
+import math
+
+file = open('input', 'r')
+
+total_fuel = 0
+rows = 0 
+
+for x in file:
+    val = math.floor(int(x) / 3) - 2 
+    while (val > 0): 
+        total_fuel += val
+        val = math.floor( val / 3) - 2 
+
+        
+
+
+print("The total fuel required is", total_fuel)
